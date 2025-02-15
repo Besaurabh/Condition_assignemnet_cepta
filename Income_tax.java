599c06b92d9147d5e675db8ed6af71b77c25698d
Saurabh Singh <135314656+Besaurabh@users.noreply.github.com>
2025-02-15 18:59:13 +0530

    Update Income_tax.java

--- Income_tax.java
+++ Income_tax.java
@@ -9,19 +9,19 @@ public class IncomeTaxCalculator {
         double tax = 0;
 
         if (income > 1000000) {
-            tax += (income - 1000000) * 0.30; // 30% above 10L
+            tax += (income - 1000000) * 0.30; 
             income = 1000000;
         }
         if (income > 500000) {
-            tax += (income - 500000) * 0.20; // 20% between 5L-10L
+            tax += (income - 500000) * 0.20; 
             income = 500000;
         }
         if (income > 250000) {
-            tax += (income - 250000) * 0.05; // 5% between 2.5L-5L
+            tax += (income - 250000) * 0.05; 
         }
 
         System.out.println("Income Tax to be paid: ₹" + tax);
 
-        sc.close();
+
     }
 }
